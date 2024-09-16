b3ea47c9bea3fdc87e4c104a54db2dbb93374f8f
Habibirrohim <164920797+Habibirrohim@users.noreply.github.com>
2024-09-16 11:40:11 +0700

    Add files via upload

---
+++ Tugasminggu2/sertifikat.js
@@ -0,0 +1,50 @@
+var book = {
+    "judul": "JavaScript",
+    "sub-judul": "Tutorial Menguasai JavaScript",
+    "sasaran": "semua pembaca",
+    penulis: {
+      namaDepan: "Sandra",
+      namaBelakang: "Dewi"
+    }
+  }
+  var skor = [100, 350, 275, 300];
+  var nilai = {bahasa: 70, matematika: 75, sains: 80};
+
+  for(var angka=10; angka>=1; angka--){
+    document.writeln(angka);
+    }
+    var angka=10;
+    while(angka>=1){
+    document.writeln(angka);
+    angka++;
+    }
+    var a = "satu";
+var c = "dua";
+function pilihJawaban(){
+  var b = "tiga";
+  var d = "empat";
+}
+var tanggal = new Date(); // Membuat objek tanggal dengan tanggal dan waktu saat ini
+var tanggalSpesifik = new Date('2024-09-16'); // Membuat objek tanggal dengan tanggal tertentu
+
+let str = "Hello";
+str += " World"; // str sekarang "Hello World"
+
+let bool = true;
+bool += 1; // bool sekarang 2 (karena true dikonversi menjadi 1)
+var nilai = 90;
+if (nilai >= 80) {
+    console.log('Lulus');
+} else {
+    console.log('Tak Lulus');
+}
+
+buah.pop();
+
+var namaPengguna;
+
+var a = "123";
+var b = 123;
+alert(a + b);
+
+Boolean(10 > 9)
